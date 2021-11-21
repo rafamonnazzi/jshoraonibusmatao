cbb901ecec048e5f70a7c317597acfcb89091d6c
Rafael <rafinhamonnazzi@hotmail.com>
2021-11-21 14:42:11 -0300

    Inserindo JavaScript no nosso projeto

---
+++ micromodal.js
@@ -0,0 +1,14 @@
+function acao(){
+
+    let modal = document.querySelector('.modal')
+
+    modal.style.display='block';
+}
+
+function fechar(){
+
+    let modal = document.querySelector('.modal')
+
+    modal.style.display='none';
+}
+
